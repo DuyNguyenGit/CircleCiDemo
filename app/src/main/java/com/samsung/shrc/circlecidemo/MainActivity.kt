cb5a525410399efe34ffaa96ac672eaa5ed9c63c
Nguyen Van Duy <nguyenvanduy.vtk33dlu@gmail.com>
2021-01-23 20:47:32 +0700

    add log

--- app/src/main/java/com/samsung/shrc/circlecidemo/MainActivity.kt
+++ app/src/main/java/com/samsung/shrc/circlecidemo/MainActivity.kt
@@ -2,10 +2,13 @@ package com.samsung.shrc.circlecidemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    val TAG = MainActivity::class.java.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate: >>>")
     }
 }
